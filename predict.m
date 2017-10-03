fc9e8176e5059ab8e711a64fb9acbd72c9fd14fa
Sebas95 <sgonzalezquesada1@gmail.com>
2017-10-03 11:27:40 -0600

    Function predict(W1,W2,X) done!

--- predict.m
+++ predict.m
@@ -14,12 +14,13 @@ function y=predict(W1,W2,X)
   # X:  Input vector, extended at its end with a 1
     
   
-  X1 = [ ones(rows(X),1)  X(:,1)  X(:,2)];
-  Arg = W1 .* X1;
-  
-  Arg2 = W2   .* 1.0 ./ (1.0 + exp(-Arg));
-  
-  y =  1.0 ./ (1.0 + exp(-Arg2));
+ X1 = [ ones(rows(X),1)  X(:,1)  X(:,2)];
+ 
+ arg1 = sigmoid( W1*X1'  );
+ 
+ y = sigmoid( W2 * [ ones(1,columns(arg1)) ; arg1] );
 endfunction;
 
-  
+ function g=sigmoid(z)
+    g = 1.0 ./ (1.0 + exp(-z));
+endfunction;
--- prueba.m
+++ prueba.m
@@ -9,13 +9,27 @@ function g=sigmoid(z)
 endfunction;
  
   
- W1 = ones(1000,3);
- W2 = ones(1000,3);
+ W1 = ones(5,3);
+ W2 = ones(3,6);
  
+ #X = [ 1 2 3];
  X = [ ones(rows(X),1)  X(:,1)  X(:,2)];
- Arg = W1 .* X;
+ 
+ arg1 = sigmoid( W1*X'  );
+ 
+ y = sigmoid( W2 * [ ones(1,columns(arg1)) ; arg1] );
   
- Arg2 = W2   .* sigmoid(Arg);
+ #Arg2 = W2   .* sigmoid(Arg);
 
- y = sigmoid(Arg2);
+ #y = sigmoid(Arg2);
   
+#______________________________Gradient of J____________________________________
+# Analytical solution.
+#
+# For each theta row (assumed in a row of the theta matrix) it will
+# compute also a row with the gradient: the first column is the partial
+# derivative w.r.t theta_0 and the second w.r.t theta_1
+function res=gradJ(theta,X,Y)
+  res=(X'*(X*theta'-Y*ones(1,rows(theta))))';
+endfunction;
+
