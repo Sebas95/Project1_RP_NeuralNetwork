620af807f1f53f314e6cab875330cbb6675f7a40
Sebas95 <sgonzalezquesada1@gmail.com>
2017-10-11 12:12:04 -0600

    MVC working for MNIST

--- MNIST/MNIST_SVM/appPredictions.py
+++ MNIST/MNIST_SVM/appPredictions.py
@@ -4,6 +4,11 @@ import numpy
 import matplotlib.pyplot as plt
 import cv2
 import sys
+import cv2
+from sklearn.externals import joblib
+from skimage.feature import hog
+import numpy as np
+from scipy.misc import *
 
 
 """
@@ -13,6 +18,22 @@ Train a SVM to categorize 28x28 pixel images into digits (MNIST dataset).
 import numpy as np
 from sklearn.externals import joblib
 from sklearn.svm import SVC
+from sklearn import datasets, svm, metrics
+
+def view_image(image, label=""):
+    """
+    View a single image.
+
+    Parameters
+    ----------
+    image : numpy array
+        Make sure this is of the shape you want.
+    label : str
+    """
+    from matplotlib.pyplot import show, imshow, cm
+    print("Label: %s" % label)
+    imshow(image, cmap=cm.gray)
+    show()
 
 
 #load the presaved model
@@ -22,19 +43,17 @@ clf = joblib.load('model.pkl')
 nameOfFile = 'six28.png'
 print(nameOfFile)
 
-#Load an image two predict it
-test = cv2.imread(nameOfFile)
-#Treat it a little two improved classification
-test = cv2.cvtColor( test, cv2.COLOR_RGB2GRAY)
-test = test.reshape(1, 1, 28, 28)
-test = cv2.bitwise_not(test)
+img = cv2.imread(nameOfFile)
+img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
+img = cv2.bitwise_not(img)
+img = img.reshape( 1,1,28, 28)
 
 
 
-#Predict the model class
-predicted = clf.predict(test)
-#Print the class predicted
-print(predicted)
+img = img[0,0,:]
 
-#http://hanzratech.in/2015/02/24/handwritten
-#-digit-recognition-using-opencv-sklearn-and-python.html
+img = img/255.0*2 - 1  #data normalization
+
+predicted = clf.predict(img.reshape((1,img.shape[0]*img.shape[1] )))
+print predicted
+
