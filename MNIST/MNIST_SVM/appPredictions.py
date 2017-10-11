5eba9594cc38e335f5abe72e6b6b3d030e7b942a
Sebas95 <sgonzalezquesada1@gmail.com>
2017-10-11 16:08:21 -0600

    readme

--- MNIST/MNIST_SVM/appPredictions.py
+++ MNIST/MNIST_SVM/appPredictions.py
@@ -5,55 +5,43 @@ import matplotlib.pyplot as plt
 import cv2
 import sys
 import cv2
-from sklearn.externals import joblib
 from skimage.feature import hog
-import numpy as np
-from scipy.misc import *
-
-
-"""
-Train a SVM to categorize 28x28 pixel images into digits (MNIST dataset).
-"""
-
 import numpy as np
 from sklearn.externals import joblib
 from sklearn.svm import SVC
 from sklearn import datasets, svm, metrics
 
-def view_image(image, label=""):
-    """
-    View a single image.
+def view_image(image):
 
-    Parameters
-    ----------
-    image : numpy array
-        Make sure this is of the shape you want.
-    label : str
-    """
     from matplotlib.pyplot import show, imshow, cm
-    print("Label: %s" % label)
     imshow(image, cmap=cm.gray)
     show()
 
 
 #load the presaved model
 clf = joblib.load('model.pkl')
+#take the image name
+nameOfFile =sys.argv[1]
 
-#nameOfFile =sys.argv[1]
-nameOfFile = 'six28.png'
 print(nameOfFile)
 
+#convert image into matrix
 img = cv2.imread(nameOfFile)
+img_ori = img
 img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
-img = cv2.bitwise_not(img)
+img = cv2.bitwise_not(img) 
 img = img.reshape( 1,1,28, 28)
 
 
-
+#convert image to array
 img = img[0,0,:]
 
-img = img/255.0*2 - 1  #data normalization
+#data normalization used in trainning process
+img = img/255.0*2 - 1  
 
 predicted = clf.predict(img.reshape((1,img.shape[0]*img.shape[1] )))
-print predicted
+print "Digit on the image predicted is: "
+print  predicted
+view_image(img_ori )
+
 
--- MNIST/MNIST_SVM/digits_classification.py
+++ MNIST/MNIST_SVM/digits_classification.py
@@ -27,51 +27,6 @@ def main():
     clf.fit(data['train']['X'][:examples], data['train']['y'][:examples])
     joblib.dump(clf, 'model.pkl') 
     
-    analyze(clf, data)
-
-
-def analyze(clf, data):
-    """
-    Analyze how well a classifier performs on data.
-
-    Parameters
-    ----------
-    clf : classifier object
-    data : dict
-    """
-    # Get confusion matrix
-    from sklearn import metrics
-    predicted = clf.predict(data['test']['X'])
-    print("Confusion matrix:\n%s" %
-          metrics.confusion_matrix(data['test']['y'],
-                                   predicted))
-    print("Accuracy: %0.4f" % metrics.accuracy_score(data['test']['y'],
-                                                     predicted))
-
-    # Print example
-    #try_id = 1
-    #out = clf.predict(data['test']['X'][try_id])  # clf.predict_proba
-    #print("out: %s" % out)
-    #size = int(len(data['test']['X'][try_id])**(0.5))
-    #view_image(data['test']['X'][try_id].reshape((size, size)),
-     #          data['test']['y'][try_id])
-    
-
-def view_image(image, label=""):
-    """
-    View a single image.
-
-    Parameters
-    ----------
-    image : numpy array
-        Make sure this is of the shape you want.
-    label : str
-    """
-    from matplotlib.pyplot import show, imshow, cm
-    print("Label: %s" % label)
-    imshow(image, cmap=cm.gray)
-    show()
-
 
 def get_data():
     """
@@ -81,54 +36,19 @@ def get_data():
     -------
     dict
     """
-    simple = False
-    if simple:  # Load the simple, but similar digits dataset
-        from sklearn.datasets import load_digits
-        from sklearn.utils import shuffle
-        digits = load_digits()
-        x = [np.array(el).flatten() for el in digits.images]
-        y = digits.target
-
-        # Scale data to [-1, 1] - This is of mayor importance!!!
-        # In this case, I know the range and thus I can (and should) scale
-        # manually. However, this might not always be the case.
-        # Then try sklearn.preprocessing.MinMaxScaler or
-        # sklearn.preprocessing.StandardScaler
-        x = x/255.0*2 - 1
-
-        x, y = shuffle(x, y, random_state=0)
-
-        from sklearn.cross_validation import train_test_split
-        x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                                            test_size=0.33,
-                                                            random_state=42)
-        data = {'train': {'X': x_train,
-                          'y': y_train},
-                'test': {'X': x_test,
-                         'y': y_test}}
-    else:  # Load the original dataset
-        from sklearn.datasets import fetch_mldata
-        from sklearn.utils import shuffle
-        mnist = fetch_mldata('MNIST original')
-
-        x = mnist.data
-        y = mnist.target
-
-        # Scale data to [-1, 1] - This is of mayor importance!!!
-        x = x/255.0*2 - 1
-
-        x, y = shuffle(x, y, random_state=0)
-
-        from sklearn.cross_validation import train_test_split
-        x_train, x_test, y_train, y_test = train_test_split(x, y,
-                                                            test_size=0.33,
-                                                            random_state=42)
-        data = {'train': {'X': x_train,
-                          'y': y_train},
-                'test': {'X': x_test,
-                         'y': y_test}}
+    # Load the original dataset
+    from sklearn.datasets import fetch_mldata
+    from sklearn.utils import shuffle
+    mnist = fetch_mldata('MNIST original')
+    x = mnist.data
+    y = mnist.target
+    # Normalization: Scale data to [-1, 1] - This is of mayor importance!!!
+    x = x/255.0*2 - 1
+    x, y = shuffle(x, y, random_state=0)
+    from sklearn.cross_validation import train_test_split
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+    data = {'train': {'X': x_train,'y': y_train}, 'test': {'X': x_test, 'y': y_test}}
     return data
-
 
 if __name__ == '__main__':
     main()
