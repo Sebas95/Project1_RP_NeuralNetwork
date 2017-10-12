821e5a21a22877c4fa923651a504599e6b62edac
geova-25 <gjosue253@gmail.com>
2017-10-11 21:38:59 -0600

    Basic change to confussion matrix

--- MNIST/Deep Neural/modelConfusionMatrix.py
+++ MNIST/Deep Neural/modelConfusionMatrix.py
@@ -23,29 +23,12 @@ from sklearn.metrics import classification_report
 
 model = load_model('my_model.h5')
 
-#For to get all image in the list of  names for image test
-
-contador = 0
-for value in namesList:
-    newImg = cv2.imread(value)
-    #Treat it a little two improved classification
-    newImg = cv2.cvtColor( newImg, cv2.COLOR_RGB2GRAY)
-    newImg = newImg.reshape(1, 1, 28, 28)
-    newImg = cv2.bitwise_not(newImg)
-    if(contador == 0):
-        arrayImages = np.array(newImg)
-    else:
-        arrayImages = np.append(newImg,arrayImages)
-    contador = contador + 1
-
-# reshape to be [samples][pixels][width][height]
-arrayImages = arrayImages.reshape(len(true),1,28,28).astype('float32')
-arrayImages.reshape(len(true),1,28,28).astype('float32')
 
 #Print confusion_matrix
 print("The confussion matrix for the data test is")
 print "\n"
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
+# reshape to be [samples][pixels][width][height]
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
 y_pred = model.predict_classes(X_test) # This will take a few seconds...
 y_true = np_utils.to_categorical(y_test)
